b05da5f3367ac00c935f98a5f818599d85befece
unknown <diego_lider@hotmail.com>
2021-02-25 09:46:51 -0500

    ok

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -21,13 +21,12 @@
           <div class="small-box bg-aqua">
             <div class="inner">
               <h3>10</h3>
-
               <p>Casos Registrados</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="<?php echo base_url();?>treatment" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url();?>customers" class="small-box-footer">Más info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -36,13 +35,12 @@
           <div class="small-box bg-green">
             <div class="inner">
               <h3>53<sup style="font-size: 20px">%</sup></h3>
-
               <p>Bounce Rate</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="#" class="small-box-footer">Más info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -51,13 +49,12 @@
           <div class="small-box bg-yellow">
             <div class="inner">
               <h3><?php echo $total_paciente; ?></h3>
-
               <p>Clientes Registrados</p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="<?php echo base_url();?>patient" class="small-box-footer">Mas info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url();?>customers" class="small-box-footer">Mas info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -66,7 +63,6 @@
           <div class="small-box bg-red">
             <div class="inner">
               <h3><?php echo $total_event; ?></h3>
-
               <p>Citas Registradas</p>
             </div>
             <div class="icon">
